ecd8416229934806b1901dd044fa83ffe388194d
appkr <juwonkim@me.com>
2020-09-28 20:54:40 +0900

    added demo1 and demo2 api

--- demo1/src/main/java/dev/appkr/demo1/web/rest/Demo1ApiDelegateImpl.java
+++ demo1/src/main/java/dev/appkr/demo1/web/rest/Demo1ApiDelegateImpl.java
@@ -1,3 +1,14 @@
 package dev.appkr.demo1.web.rest;
-public class Demo1ApiDelegateImpl {
+
+import dev.appkr.demo1.web.api.Demo1ApiDelegate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo1ApiDelegateImpl implements Demo1ApiDelegate {
+
+  @Override
+  public ResponseEntity<String> demo1() {
+    return ResponseEntity.ok("This is demo1");
+  }
 }
--- demo2/src/main/java/dev/appkr/demo2/web/rest/Demo2ApiDelegateImpl.java
+++ demo2/src/main/java/dev/appkr/demo2/web/rest/Demo2ApiDelegateImpl.java
@@ -1,3 +1,14 @@
 package dev.appkr.demo2.web.rest;
-public class Demo2ApiDelegateImpl {
+
+import dev.appkr.demo2.web.api.Demo2ApiDelegate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo2ApiDelegateImpl implements Demo2ApiDelegate {
+
+  @Override
+  public ResponseEntity<String> demo2() {
+    return ResponseEntity.ok("This is demo2");
+  }
 }
