30e8632273b9e0c71caf9f6a0032f4ae08bf667a
appkr <juwonkim@me.com>
2020-09-29 10:29:47 +0900

    make Demo1ApiDelegateImpl call Demo2Api NOT WORKING

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 DIR=$PWD
 
-cd $DIR/uaa && ./gradlew clean -x test build -Pprod jibDockerBuild ;
-cd $DIR/demo1 && ./gradlew clean -x test build -Pprod jibDockerBuild ;
-cd $DIR/demo2 && ./gradlew clean -x test build -Pprod jibDockerBuild ;
+cd $DIR/uaa && ./gradlew clean build -x test -x integrationTest -Pprod jibDockerBuild ;
+cd $DIR/demo1 && ./gradlew clean build -x test -x integrationTest -Pprod jibDockerBuild ;
+cd $DIR/demo2 && ./gradlew clean build -x test -x integrationTest -Pprod jibDockerBuild ;
--- demo1/src/main/java/dev/appkr/demo1/web/rest/Demo1ApiDelegateImpl.java
+++ demo1/src/main/java/dev/appkr/demo1/web/rest/Demo1ApiDelegateImpl.java
@@ -1,14 +1,30 @@
 package dev.appkr.demo1.web.rest;
 
 import dev.appkr.demo1.web.api.Demo1ApiDelegate;
+import dev.appkr.demo2.java.api.Demo2Api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 
 @Component
 public class Demo1ApiDelegateImpl implements Demo1ApiDelegate {
 
+  private final Demo2Api demo2Api;
+
+  public Demo1ApiDelegateImpl(Demo2Api demo2Api) {
+    this.demo2Api = demo2Api;
+  }
+
   @Override
   public ResponseEntity<String> demo1() {
-    return ResponseEntity.ok("This is demo1");
+    String message = "This is demo1";
+
+    try {
+      message = demo2Api.demo2();
+    } catch (RestClientException e) {
+      e.printStackTrace();
+    }
+
+    return ResponseEntity.ok(message);
   }
 }
